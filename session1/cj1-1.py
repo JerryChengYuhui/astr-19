53d69d128f50e9c047c6edf4ae260acb59f69203
JerryChengYuhui <157863321+JerryChengYuhui@users.noreply.github.com>
2024-01-27 15:42:10 -0800

    Add files via upload

---
+++ session1/cj1-1.py
@@ -0,0 +1,11 @@
+#Jerry Cheng cj1-1
+
+name = "Jerry Cheng" # Create a variable 'name' and assign it the value "Jerry Cheng"
+pronouns = "he/him" # Create a variable 'pronouns' and assign it the value "he/him"
+favorite_movie = "Fast & Furious 7" # Create a variable 'favorite_movie' and assign it the value "Fast & Furious 7"
+favorite_food = "burger" # Create a variable 'favorite_food' and assign it the value "burger"
+
+print(f"My name is {name}.") # Print "My name is Jerry Cheng.", where {name} is replaced with the value of the 'name' variable
+print(f"My preferred pronouns are {pronouns}].") # Print "My preferred pronouns are he/him.", where {pronouns} is replaced with the value of the 'pronouns' variable
+print(f"My favorite movie is {favorite_movie}.") # Print "My favorite movie is Fast & Furious 7.", where {favorite_movie} is replaced with the value of the 'favorite_movie' variable
+print(f"My favorite food is {favorite_food}.") # Print "My favorite food is burger.", where {favorite_food} is replaced with the value of the 'favorite_food' variable
